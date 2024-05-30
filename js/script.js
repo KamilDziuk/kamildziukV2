2f31966e6211729e9812f7057394731de068958e
kamildziuk <kontakt@kamildziuk.pl>
2024-05-30 10:32:38 +0200

    kamildziuk.github.io version 2

---
+++ js/script.js
@@ -0,0 +1,60 @@
+
+function ScrollMenu()
+{
+  
+    this.aboutMeText =  () =>
+    {
+        this.contaktText();
+        this.projectsText();
+window.addEventListener("scroll", () => {
+    let scroll = window.pageYOffset || document.documentElement.scrollaboutMeText;
+    if (scroll >= 60) {
+    document.querySelector(".aboutMeText").classList.add("aboutMeText--scrolling");
+    }
+    else {
+    document.querySelector(".aboutMeText").classList.remove("aboutMeText--scrolling");
+    document.querySelector(".projectsText").classList.remove("projectsText--scrolling");
+    }  
+    });
+    }
+    this.projectsText=  () =>
+    {
+       
+        window.addEventListener("scroll", () => {
+            let scroll = window.pageYOffset || document.documentElement.scrollprojectsText;
+
+     if(scroll >=110)
+        {
+            document.querySelector(".projectsText").classList.add("projectsText--scrolling");
+        } 
+    
+    else {
+  
+    document.querySelector(".projectsText").classList.remove("projectsText--scrolling");
+    } ;
+    })
+
+    }
+
+    this.contaktText=  () =>
+    {
+     window.addEventListener("scroll", () => {
+        let scroll = window.pageYOffset || document.documentElement.scrollcontaktText;
+
+     if(scroll >= 170)
+        {
+            document.querySelector(".contaktText").classList.add("contaktText--scrolling");
+        } 
+    
+    else {
+  
+    document.querySelector(".contaktText").classList.remove("contaktText--scrolling");
+    }
+    });   
+    }
+}
+
+let scrollMenuResult = new ScrollMenu();
+
+
+scrollMenuResult.aboutMeText();
